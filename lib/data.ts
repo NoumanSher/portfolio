ef5fa19293864197d053d00e97eabfb49409c97c
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2025-02-24 09:44:45 +0500

    change

--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,9 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+
 import hnl from "@/public/hnl.png";
 import realEstate from "@/public/realtor.png";
 import aurum from "@/public/aurum.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -41,7 +39,7 @@ export const experiencesData = [
     description:
       "As a Frontend Developer at Averlabz, I worked on an extensive ERP system project for two years. This system was designed to streamline the organization’s daily operations, including human resources, inventory, and supply chain management. It featured various modules for user management, attendance management, site management, and more.The project was built on the ABP boilerplate. My primary responsibilities were on the frontend side, where I utilized React, TypeScript, and MobX to create an interactive and user-friendly interface. Although .NET was used for the backend, my focus remained on frontend development.This experience allowed me to contribute to a leading MS vendor in the country, working with all major telecom operators in Pakistan, and maintaining around 12,000 BTS and 20 Core sites. It was an opportunity to apply and enhance my skills in a real-world, large-scale project.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019",
+    date: "2022",
   },
   {
     title: "AGRI-BANK",
@@ -65,7 +63,7 @@ export const experiencesData = [
     description:
       "As a Frontend Developer, I was instrumental in building the user interface using React and Next.js. I also utilized Tailwind CSS for styling and React Query for state management. My contributions ensured a seamless and user-friendly experience, making the process of purchasing authentic items easy and enjoyable.",
     icon: React.createElement(CgWorkAlt),
-    date: "2024 - Present",
+    date: "2024",
   },
   {
     title: "Real Estate",
@@ -73,7 +71,7 @@ export const experiencesData = [
     description:
       "With four years of expertise, I’ve crafted a platform that not only meets the needs of prospective homeowners but also enhances their property search experience. The application is developed with a tech stack that includes React, TypeScript, Next.js, Tailwind, React Query, and ReSend, demonstrating my proficiency in these technologies.This project has been a testament to my ability to combine technical skills with a deep understanding of user needs, resulting in a product that transforms the way users explore properties.",
     icon: React.createElement(CgWorkAlt),
-    date: "2024 - Present",
+    date: "2024",
   },
 ] as const;
 
@@ -82,17 +80,24 @@ export const projectsData = [
     title: "HNL",
     description:
       "Hitech Networks Ltd. HNL is one of the leading MS vendors in the country, working with all major telecom operators in Pakistan, maintaining around 12000 BTS and 20 Core sites. HNL project is an ERP system that helps an organization manage its day-to-day operations, including human resources, inventory, and supply chain management. The system includes modules for user management, attendance management, site management and many more.",
-    tags: ["React", "Tailwind",'ReSend','Redux'],
+    tags: ["React", "Tailwind", "ReSend", "Redux"],
     imageUrl: hnl,
-    url:''
+    url: "",
   },
   {
     title: "Real Estate App",
     description:
       "Embark on a seamless property exploration journey with my Real Estate App. Developed with precision using React and Next.js, this application delivers an immersive experience for users seeking their dream homes. With four years of expertise, I've crafted a platform that not only showcases properties elegantly but also ensures a user-friendly interface.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "React Query","ReSend"],
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind",
+      "React Query",
+      "ReSend",
+    ],
     imageUrl: realEstate,
-    url:"https://realtor-web-drab.vercel.app"
+    url: "https://realtor-web-drab.vercel.app",
   },
   {
     title: "Aurum",
@@ -100,7 +105,7 @@ export const projectsData = [
       "Aurum is a unique online marketplace where you can purchase authentic items belonging to world-famous personalities. From T-shirts of renowned footballers like Cristiano Ronaldo, Aurum offers a variety of products. What sets Aurum apart is the ability to verify the authenticity of every product, ensuring that what you’re buying is 100% original. Aurum guarantees both authenticity and passion in every purchase.",
     tags: ["React", "Redux", "Tailwind"],
     imageUrl: aurum,
-    url:'https://www.aurummt.com'
+    url: "https://www.aurummt.com",
   },
 ] as const;
 
@@ -115,6 +120,5 @@ export const skillsData = [
   "Tailwind",
   "Api integration",
   "Redux",
-  "GraphQL",
   "Framer Motion",
 ] as const;
