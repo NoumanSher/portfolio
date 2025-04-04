0f5441a5524a546c8dc5a0a6ebf8c067817be4de
NoumanSher <75526797+NoumanSher@users.noreply.github.com>
2025-04-04 22:01:05 +0500

    change

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 
 import hnl from "@/public/hnl.png";
+import namKart from "@/public/namKart.webp";
 import realEstate from "@/public/realtor.png";
 import aurum from "@/public/aurum.png";
 
@@ -34,48 +35,24 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Hitech Networks Ltd",
+    title: "Averlabz",
     location: "Pakistan, Lahore",
     description:
-      "As a Frontend Developer at Averlabz, I worked on an extensive ERP system project for two years. This system was designed to streamline the organization’s daily operations, including human resources, inventory, and supply chain management. It featured various modules for user management, attendance management, site management, and more.The project was built on the ABP boilerplate. My primary responsibilities were on the frontend side, where I utilized React, TypeScript, and MobX to create an interactive and user-friendly interface. Although .NET was used for the backend, my focus remained on frontend development.This experience allowed me to contribute to a leading MS vendor in the country, working with all major telecom operators in Pakistan, and maintaining around 12,000 BTS and 20 Core sites. It was an opportunity to apply and enhance my skills in a real-world, large-scale project.",
+      "Experienced React.js developer with 3 years of expertise in building dynamic, responsive web applications. Proficient in Next.js, Redux, Ant Design, and Tailwind CSS for creating scalable UIs.Strong background in state management, API integration, performance optimization, and SEO best practices. Skilled in leveraging React hooks, context API, and @tanstack/react-query for efficient data fetching and handling.I am dedicated to fixing front-end issues to enhance both the functionality and overall user experience of the platforms.",
     icon: React.createElement(CgWorkAlt),
     date: "2022",
-  },
-  {
-    title: "AGRI-BANK",
-    location: "Pakistan, Lahore",
-    description:
-      "As a Frontend Developer on the AGRI-BANK project, I contributed to the development of a loan-based application tailored specifically for farmers. My primary responsibility was the frontend development, where I utilized React.js with TypeScript and MobX for state management.During my tenure, I collaborated closely with the team to translate project requirements into intuitive user interfaces. I played a key role in implementing features that allowed farmers to calculate the approximate costs of seeding, fertilizer, and pesticide usage, as well as estimating crop yield based on land size and crop type.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2022",
-  },
-  {
-    title: "Madam Portal",
-    location: "Pakistan, Lahore",
-    description:
-      "I worked on a project called ‘Madam Portal’, an innovative platform that allows users to rent high-end clothing for a specific duration. This service is particularly beneficial for those who need expensive attire, such as bridal or party suits, for a short period. The actual price of these clothes can be quite high, making this rental service a cost-effective solution. Madam Portal offers flexible subscription plans, including one-month and three-month options, providing users with access to a wide range of premium clothing. This project was a unique opportunity to combine technology and fashion, providing a practical solution to a common problem",
-    icon: React.createElement(CgWorkAlt),
-    date: "2023",
-  },
-  {
-    title: "Aurum",
-    location: "Pakistan, Lahore",
-    description:
-      "As a Frontend Developer, I was instrumental in building the user interface using React and Next.js. I also utilized Tailwind CSS for styling and React Query for state management. My contributions ensured a seamless and user-friendly experience, making the process of purchasing authentic items easy and enjoyable.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2024",
-  },
-  {
-    title: "Real Estate",
-    location: "Pakistan, Lahore",
-    description:
-      "With four years of expertise, I’ve crafted a platform that not only meets the needs of prospective homeowners but also enhances their property search experience. The application is developed with a tech stack that includes React, TypeScript, Next.js, Tailwind, React Query, and ReSend, demonstrating my proficiency in these technologies.This project has been a testament to my ability to combine technical skills with a deep understanding of user needs, resulting in a product that transforms the way users explore properties.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2024",
   },
 ] as const;
 
 export const projectsData = [
+  {
+    title: "NamKart",
+    description:
+      "This project is engineered to deliver a high-performance, responsive online shopping environment, where every UI element and API interaction is optimized for speed and reliability, ensuring an intuitive and efficient experience for users.",
+    tags: ["Next js", "Tailwind", "Typescript", "React Query"],
+    imageUrl: namKart,
+    url: "https://market-place-neon.vercel.app/",
+  },
   {
     title: "HNL",
     description:
@@ -97,7 +74,7 @@ export const projectsData = [
       "ReSend",
     ],
     imageUrl: realEstate,
-    url: "https://realtor-web-drab.vercel.app",
+    url: "https://real-esatate-three.vercel.app/",
   },
   {
     title: "Aurum",
@@ -105,7 +82,7 @@ export const projectsData = [
       "Aurum is a unique online marketplace where you can purchase authentic items belonging to world-famous personalities. From T-shirts of renowned footballers like Cristiano Ronaldo, Aurum offers a variety of products. What sets Aurum apart is the ability to verify the authenticity of every product, ensuring that what you’re buying is 100% original. Aurum guarantees both authenticity and passion in every purchase.",
     tags: ["React", "Redux", "Tailwind"],
     imageUrl: aurum,
-    url: "https://www.aurummt.com",
+    url: "",
   },
 ] as const;
 
